89e71a9bc29538a24226a554f6fb2f4debc69ea6
Paul Schwendenman <paul@paul-server0>
2010-10-24 20:35:41 -0400

    added more documentation,  added class bid and table. need to: - finish the value system - finish ai play and bidding - rework system... look at examples?

--- euchre.py
+++ euchre.py
@@ -14,6 +14,10 @@ import random
 
 # Constants fo the euchre game.
 ranks = [None, "9", "10", "J", "Q", "K", "A",]
+cardname = [None, "9", "10", "J", "Q", "K", "A", "9T", "10T", "QT", "KT", "AT", "LB", "JT"]
+cardvalu = [  -1,  1 ,   2 ,  3 ,  4 ,  5 ,  10,  12 ,   15 ,  20 ,  25 ,  30 ,  31 ,  35 ]
+values = dict(zip(cardname, cardvalu))
+left = { "S" : "C", "C" : "S", "D" : "H", "H" : "D"}
 suits = [None, "S", "D", "C", "H"]
 suit_chars = [None, unichr(9824), unichr(9827), unichr(9829), unichr(9830)]
 #suit_chars = [None, 9824, 9827, 9829, 9830]
@@ -27,23 +31,37 @@ class card:
         self.ranks = ranks
         self.suits = suits
     def getRank(self):
+        """returns the rank of the card, i.e. 9"""
+        """remove?"""
         return self.rank
     def getSuit(self):
+        """returns the suit of the card, i.e. S"""
+        """remove?"""
         return self.suit
     def value(self, trump, lead = None):
-        try:
-            value = ranks.index(self.rank)
-        except:
-            print self.rank, "is not in", ranks
-            exit()
+        """Calculates the value of the card bassed on trump and a lead?
+        better was to do this?"""
         if (self.suit == trump):
-            value += 20
-            if (self.rank == "J"):
-                value = 28
-        elif (((suits.index(self.suit) + suits.index(trump)) % 2 == 0) and self.rank == "J"):
-            value = 27
-        elif (self.suit == lead):
-            value += 10
+            value = values[self.rank + "T"]
+        elif (self.rank == "J" and trump == left[self.suit]):
+            value = values["LB"]
+        else:
+            value = values[self.rank]
+            
+
+#        try:
+#            value = ranks.index(self.rank)
+#        except:
+#            print self.rank, "is not in", ranks
+#            exit()
+#        if (self.suit == trump):
+#            value += 20
+#            if (self.rank == "J"):
+#                value = 28
+#        elif (((suits.index(self.suit) + suits.index(trump)) % 2 == 0) and self.rank == "J"):
+#            value = 27
+#        elif (self.suit == lead):
+#            value += 10
 
         #This print statement is for debugging
         #print "rank: %2i card: %s trump %s" % (value, self, trump)
@@ -58,11 +76,13 @@ class card:
 #        output += "%2s of" % (self.rank) + suit_chars[suits.index(self.suit)] + "\033[m"
         return output
 
-class hand(object):
+
+class hand():
     """Hand is used as the base class for all the rest of the card handlers. Hand of playing cards, base class __init__, __str__, clear, add, remove, and give"""
     def __init__(self):
         self.index = 0
         self.cards = []
+        self.name = ""
     def __str__(self):
         if self.cards:
             rep = ""
@@ -81,13 +101,16 @@ class hand(object):
         self.cards.remove(card)
         other_hand.add(card)
     def bubble_sort(self, trump = None):
-        "Sorts list in place and returns it."
+        "Sorts list in place and returns it. Either by trump if given or by suit."
         cards = self.cards
         for passesLeft in range(len(cards)-1, 0, -1):
             for index in range(passesLeft):
-                if ((cards[index] < cards[index + 1] and trump == None) or 
-                (cards[index].value(trump) < cards[index + 1].value(trump) and trump != None)):
-                   cards[index], cards[index + 1] = cards[index + 1], cards[index]
+                if (trump == None):
+                    if (cards[index] < cards[index + 1]):
+                       cards[index], cards[index + 1] = cards[index + 1], cards[index]
+                else:
+                    if (cards[index].value(trump) < cards[index + 1].value(trump)):
+                       cards[index], cards[index + 1] = cards[index + 1], cards[index]
         self.cards = cards
     def search(self, suit = None, rank = None):
         cards = self.cards[:]
@@ -109,14 +132,13 @@ class trick(hand):
         # This print statement is for debugging
         #print best_card, "is #", this + 1
         return self.cards.index(best_card)
+
 class deck(hand):
     """Deck is inherited from Hand. has: populate, deal, and shuffle"""
     def populate(self):
         """Builds the deck with cards"""
         for suit in suits[1:]:
-#        for suit in range(len(suits)):
             for rank in ranks[1:]:
-#            for rank in range(len(ranks))[1:]:
                 self.add(card(rank, suit))
 
     def shuffle(self):
@@ -132,11 +154,9 @@ class deck(hand):
                 self.give(top_card, hands[((dealer + index) % 4)])
             except:
                 print "Can't continue deal. Out of cards!"
-#        for hand in hands:
         for index in range(1, 5):
             try:
                 top_card = self.cards[0]
-#                self.give(top_card, hand)
                 self.give(top_card, hands[((dealer + index) % 4)])
                 top_card = self.cards[0]
                 self.give(top_card, hands[((dealer + index) % 4)])
@@ -146,7 +166,7 @@ class deck(hand):
                 print "Can't continue deal. Out of cards!"
         
 class player(hand):
-    """Player is inherited from hand, is interactive. Has set_hand, get_play, get_bid, pick_it_up"""
+    """Player is inherited from trick, is interactive. Has set_hand, get_play, get_bid, pick_it_up, worst_card and highest_nontrump"""
     def set_hand(self, hand):
         """Set_hand assigns the cards to the hand"""
         self.cards = hand.cards
@@ -177,7 +197,7 @@ class player(hand):
             if (i == card):
                 return self.cards[index]
             index += 1
-    def get_bid(self, top_card = 0, dealer = 0):
+    def bid(self, top_card = 0, dealer = 0):
         if(top_card == 0):
             #print "\033[2J\033[0;0H",
             print "\nYour hand", self.index + 0, "\033[1D:", self
@@ -230,6 +250,32 @@ class player(hand):
             if (i == card):
                 self.remove(self.cards[index])
             index += 1
+    def worst_card(self, trump):
+        lead = self.cards[0].suit
+        best_card = self.cards[0]
+        best_value = best_card.value(trump, lead)
+        for each_card in self.cards:
+            if(each_card.value(trump, lead) > best_value):
+                best_card = each_card
+                best_value = best_card.value(trump, lead)                
+        this = self.cards.index(best_card)
+        # This print statement is for debugging
+        #print best_card, "is #", this + 1
+        return self.cards.index(best_card)
+    def best_card(self, trump):
+        lead = self.cards[0].suit
+        best_card = self.cards[0]
+        best_value = best_card.value(trump, lead)
+        for each_card in self.cards:
+            if(each_card.value(trump, lead) > best_value):
+                best_card = each_card
+                best_value = best_card.value(trump, lead)                
+        this = self.cards.index(best_card)
+        # This print statement is for debugging
+        #print best_card, "is #", this + 1
+        return self.cards.index(best_card)
+
+
 class comp(player):
     """Player is inherited from hand, is a "computer" player and has limited AI. Has get_play, get_bid, pick_it_up"""
     """ Cards that it can beat vs cards that beat it"""
@@ -343,66 +389,100 @@ class comp(player):
         print "(" , self.index, ": " , self.cards[index] , ")", self
         return self.cards[index]
 
-    def get_bid(self, top_card = 0, dealer = 0):
+    def bid(self, top_card = 0, dealer = 0):
         bid = "P"
-                
-        bid = suits[random.randrange(0,5)]
-        if (bid == None or top_card != 0): bid = "P"
+        if top_card == 0:
+            """Everyone Passed"""
+            for suit in suits: 
+                if self.value(suit) > 85:
+                    bid = suit
+        elif dealer != 0:
+            """Put Intelligent bidding ie do and don't order put etc..."""
+            if (self.value(top_card.suit) > 85):
+                bid = suit        
+        else:
+            """Top Card exists, check score (stupid bidding) """
+            if (self.value(top_card.suit) > 85):
+                bid = suit
+
+#        bid = suits[random.randrange(0,5)]
+#        if (bid == None or top_card != 0): bid = "P"
+
         return bid
+
+    def value(self):
+        for card in self.cards:
+            value += card.value
+        return value    
     def pick_it_up(self, top_card):
         self.add(top_card)
         index = 0
         index = random.randrange(0, len(self.cards) - 1)
         self.remove(self.cards[index])
-class euchre:
-    """Highest level class creates a game for play"""
+class table:
+    """Class for the table, has players."""
     def __init__(self):
-        """Sets up and starts a game."""
+        pass
+
+    def start(self):
+        num_players = 0
+        self.players = []
+#            self.players = [player(), comp(), comp(), comp(), ] 
+#            self.players = [player(), player(), player(), player(), ] 
+#            self.players = [player(), comp(), player(), comp(), ] 
+        self.players = [comp(), comp(), comp(), comp(), ] 
+        self.players[0].name = "Paul"
+        self.players[1].name = "Phil"
+        self.players[2].name = "Sierra"
+        self.players[3].name = "Julia"
+
+    
+class bid:
+    """Class for each hand in a game, meaning all the stuff needed to play for one hand."""
+    def __init__(self):
+        pass
+
+
+    def start(self, players):
         bid = ""
-        dealer = random.randrange(0,4)
-#        dealer = 0
-        while(not self.good_bid(bid)):
-            my_deck = deck()
-            my_deck.populate()
-            my_deck.bubble_sort()
-            my_deck.shuffle()
-            num_players = 0
-            players = []
-#            players = [player(), comp(), comp(), comp(), ] 
-#            players = [player(), player(), player(), player(), ] 
-#            players = [player(), comp(), player(), comp(), ] 
-            players = [comp(), comp(), comp(), comp(), ] 
-            my_deck.deal(players, dealer)
+        self.dealer = random.randrange(0,4)
+#        self.dealer = 0
+        self.deck = deck()
+        self.deck.populate()
+        self.deck.bubble_sort()
+        self.deck.shuffle()
+        self.deck.deal(players, self.dealer)
 
-            for each_player in players:
-                each_player.index = players.index(each_player)
-                each_player.bubble_sort()
+        for each_player in players:
+            each_player.index = players.index(each_player)
+            each_player.bubble_sort()
 
-            top_card = my_deck.cards[0]
-            bid = self.get_bid(players, top_card, dealer)
 
+    def bid():
+        top_card = self.deck.cards[0]
+        bid = self.bid(players, top_card)
+        return bid
+    def play(self, players, bid = 'S'):
         trump = bid
         played_cards = hand()
         del bid
-        print "trump:\t\t", trump, "\ndealer:\t\t", dealer
+        print "trump:\t\t", trump, "\ndealer:\t\t", self.dealer
         for each_player in players:
             each_player.bubble_sort(trump)
-        leader = dealer + 1
-        del dealer
+        leader = self.dealer + 1
+        del self.dealer
         tricks = 5 * [trick()] #tricks = [trick(), trick(), trick(), trick(), trick(),]
         for each_trick in tricks:
             each_trick = self.get_play(players, trump, leader)
             winner = each_trick.best_card(trump)
 #            #print "\033[2J\033[0;0H",
             #This is a print statement used for debugging
-            #print "(",leader,"+",winner,") % 4 =", leader+winner, "% 4 =", (leader+winner) % 4
+             #print "(",leader,"+",winner,") % 4 =", leader+winner, "% 4 =", (leader+winner) % 4
             print "The winner was: ", (leader + winner) % 4, " = ", each_trick.cards[winner], "of", each_trick
             leader = leader + winner
         for each_trick in tricks:
             while (len(each_trick.cards) > 0):      
-                each_trick.give(played_cards.cards[0], my_deck)
-    def rank_cards():
-        pass        
+                each_trick.give(played_cards.cards[0], self.deck)
     def get_play(self, players, trump, leader):
         """Handles the card play for all players"""
         played_cards = trick()
@@ -415,28 +495,44 @@ class euchre:
         print
         return played_cards
 
-    def get_bid(self, players, top_card, dealer):
+    def get_bid(self, players, top_card):
         """Handles the bid recovery for all players"""
         for index in range(1, 5):
-            bid = players[((dealer + index) % 4)].get_bid(top_card, dealer)
+            bid = players[((self.dealer + index) % 4)].get_bid(top_card, self.dealer)
             if self.good_bid(bid):
-                players[dealer].pick_it_up(top_card)
+                players[self.dealer].pick_it_up(top_card)
                 return bid
         for index in range(1, 5):
-            bid = players[((dealer + index) % 4)].get_bid()
+            bid = players[((self.dealer + index) % 4)].get_bid()
             if self.good_bid(bid):
                 return bid
         return bid
 
-    def good_bid(self, bid):
-        """tests to see if the bid is "Good" """
-        if (bid == "S" or bid == "C" or bid == "H" or bid == "D"):
-            return 1
-        else:
-            return 0
+#    def good_bid(self, bid):
+#        """tests to see if the bid is "Good" """
+#        if (bid == "S" or bid == "C" or bid == "H" or bid == "D"):
+#            return 1
+#        else:
+#            return 0
+
+
+
+class euchre:
+    """Highest level class creates a game for play"""
+    def __init__(self):
+        """Sets up and starts a game."""
+        self.table = table()
+        self.table.start()
+        
+        self.bid = bid()
+        mybid = self.bid.start(self.table.players)
+        self.bid.play(self.table.players, mybid)
+    def rank_cards():
+        pass        
+
 def main():
     euchre()
-    
+
 if __name__ == "__main__":
     main()
 
