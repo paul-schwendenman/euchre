b46daebd08b6a95019b19f45482a75568a3ea3b3
Paul <schwendenman.paul@gmail.com>
2010-12-17 00:56:42 -0500

    got it sending dict across. start wiring a fake client and server

--- player_client.py
+++ player_client.py
@@ -1,4 +1,5 @@
 import socket
+from cPickle import dumps, loads
 
 class player_client():
     def __init__(self, index = 0):
@@ -6,21 +7,25 @@ class player_client():
         self.client_socket.connect(("localhost", 5000 + index))
 
     def chat(self):
-        while 1:
-            data = self.client_socket.recv(512)
-            if ( data == 'q' or data == 'Q'):
-                self.client_socket.close()
-                break;
+#        while 1:
+        data = loads(self.client_socket.recv(512))
+        if (data["quit"]):
+            self.client_socket.close()
+#            break;
+        else:
+            print "RECIEVED:" , data
+            data = self.display(data)
+            if (not data["quit"]):
+                self.client_socket.send(dumps(data))
             else:
-                print "RECIEVED:" , data
-                data = raw_input ( "SEND( TYPE q or Q to Quit):" )
-                if (data <> 'Q' and data <> 'q'):
-                    self.client_socket.send(data)
-                else:
-                    self.client_socket.send(data)
-                    self.client_socket.close()
-                    break;
-                    
+                self.client_socket.send(dumps(data))
+                self.client_socket.close()
+#                break;
+    def display(self, data):
+        data["msg"] = "Whats shaken?"
+        #x = raw_input()
+        return data
+
 
 if __name__ == "__main__":
     p = player_client()
--- player_server.py
+++ player_server.py
@@ -1,37 +1,42 @@
 import socket
-import sys
+from cPickle import dumps, loads
 
 port = 5643
 
 class player_server():
     def __init__(self):
-        import socket
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind(("", 5000))
         self.server_socket.listen(5)
 
-    def chat(self):
+    def chat(self, data):
         print "TCPServer Waiting for client on port 5000"
-        while 1:
-                client_socket, address = self.server_socket.accept()
-                print "I got a connection from ", address
-                while 1:
-                        data = raw_input ( "SEND( TYPE q or Q to Quit):" )
-                        if (data == 'Q' or data == 'q'):
-                                client_socket.send (data)
-                                client_socket.close()
-                                break;
-                        else:
-                                client_socket.send(data)
-         
-                        data = client_socket.recv(512)
-                        if ( data == 'q' or data == 'Q'):
-                                client_socket.close()
-                                break;
-                        else:
-                                print "RECIEVED:" , data
+#        while 1:
+        client_socket, address = self.server_socket.accept()
+        print "I got a connection from ", address
+#        while 1:
+#        data = raw_input ( "SEND( TYPE q or Q to Quit):" )
+        if (data["quit"]):
+            client_socket.send (dumps(data))
+            client_socket.close()
+#            break;
+        else:
+            client_socket.send(dumps(data))
+ 
+            data = loads(client_socket.recv(512))
+            if ( data == 'q' or data == 'Q'):
+                client_socket.close()
+#                break;
+            else:
+                print "RECIEVED:" , data
+        return data                                
                 
 if __name__ == "__main__":
+    a = {"quit":0,"msg":"yes"}
     p = player_server()
-    p.chat()
+    #x = raw_input(":")
+    print p.chat(a)
+#    x = raw_input(":")
+#    a["quit"] = 1
+#    p.chat(a)
  
